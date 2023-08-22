32b3d0f020e42b3675d1c21db3d1610081067e3b
Adam Wayne <adamwayne.dev@gmail.com>
2023-08-22 20:07:01 +0100

    Added art assets for good and bad memories

---
+++ Assets/BadMemoryMediumVariant.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadMemoryMediumVariant : MonoBehaviour
+{
+    [SerializeField] List<Sprite> sprites = new List<Sprite>();
+    SpriteRenderer currentSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentSprite = GetComponent<SpriteRenderer>();
+        currentSprite.sprite = sprites[Random.Range(0, sprites.Count)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
